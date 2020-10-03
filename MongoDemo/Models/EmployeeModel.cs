38b89890e83ef2ad39994ebedce86e496721dfd3
honeyo7 <prateek.j@outlook.com>
2020-10-03 17:03:22 +0530

    1st Commit

---
+++ MongoDemo/Models/EmployeeModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MongoDemo.Models
+{
+    public class EmployeeModel
+    {
+        public int id { get; set; }
+
+        [Required, MaxLength(50, ErrorMessage = "Name cannot exceed 50 characters")]
+        public string name { get; set; }
+        [Display(Name = "Office Email")]
+        [RegularExpression(@"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$",
+        ErrorMessage = "Invalid email format")]
+        [Required]
+        public string email { get; set; }
+    }
+}
